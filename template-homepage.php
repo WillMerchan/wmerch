ffd262513b84ab92ad6da079f5b6e839339521e3
Will Merchan <willmerchan@gmail.com>
2016-04-12 16:21:02 -0400

    Initialize

---
+++ template-homepage.php
@@ -0,0 +1,25 @@
+<?php
+/*
+Template Name: Homepage
+*/
+?>
+
+<?php get_header(); ?>
+			
+	<div id="content">
+	
+		<div id="inner-content" class="row">
+	
+		    <main id="main" class="large-12 medium-12 columns" role="main">
+				
+				<section id="home-hero">
+					<h1>Testing out HTML in PHP template</h1>
+				</section>						
+
+			</main> <!-- end #main -->
+		    
+		</div> <!-- end #inner-content -->
+	
+	</div> <!-- end #content -->
+
+<?php get_footer(); ?>
